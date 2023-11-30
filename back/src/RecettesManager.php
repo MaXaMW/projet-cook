56ca4b1fc9c79749f5cf9134d617279d8a91c603
AMANZOUGAGHENE Massinissa <m.amanzougaghene@ecole-ipssi.net>
2023-11-30 16:28:32 +0100

    modification et ajout

--- back/src/RecettesManager.php
+++ back/src/RecettesManager.php
@@ -10,21 +10,37 @@
         public function recupererToutesLesRecettes(){
             $stmt = $this->pdo->prepare("SELECT * FROM recettes");
             $stmt->execute();
-            $results = $stmt->fetchAll();
-
+            $results = $stmt->fetchAll(PDO::FETCH_ASSOC); // Utilisez FETCH_ASSOC pour obtenir un tableau associatif
+        
             $recettes = [];
             foreach ($results as $result) {
-                $recettes[] = new Recette(
-                    $result['id_recette'],
-                    $result['nom_recette'],
-                    $result['difficulte'],
-                    $result['temps_preparation'],
-                    $result['instructions'],
-                    $result['id_categorie']
-                );
+                $recettes[] = $result; // Ajoutez directement le tableau associatif
             }
-
+        
             return $recettes;
         }
+        
+
+        public function modifierRecette($id, $nom, $difficulte, $temps_preparation, $instructions, $id_categorie) {
+            $stmt = $this->pdo->prepare("UPDATE recettes SET nom_recette = :nom, difficulte = :difficulte, temps_preparation = :temps_preparation, instructions = :instructions, id_categorie = :id_categorie WHERE id_recette = :id");
+            $stmt->bindParam(':id', $id);
+            $stmt->bindParam(':nom', $nom);
+            $stmt->bindParam(':difficulte', $difficulte);
+            $stmt->bindParam(':temps_preparation', $temps_preparation);
+            $stmt->bindParam(':instructions', $instructions);
+            $stmt->bindParam(':id_categorie', $id_categorie);
+            $stmt->execute();
+        }
+
+        public function ajouterRecette($nom, $difficulte, $temps_preparation, $instructions, $id_categorie) {
+            $stmt = $this->pdo->prepare("INSERT INTO recettes (nom_recette, difficulte, temps_preparation, instructions, id_categorie) VALUES (:nom, :difficulte, :temps_preparation, :instructions, :id_categorie)");
+            $stmt->bindParam(':nom', $nom);
+            $stmt->bindParam(':difficulte', $difficulte);
+            $stmt->bindParam(':temps_preparation', $temps_preparation);
+            $stmt->bindParam(':instructions', $instructions);
+            $stmt->bindParam(':id_categorie', $id_categorie);
+            $stmt->execute();
+        }
+        
     }
     
